51aae268b6540166383ff0c1cde0ce5405e08fe2
Ali Mohammadi <alimohammadi02321@gmail.com>
2024-12-26 14:05:01 +0330

    Improve/ understand basics of promise function

--- session4/public/src/js/app.js
+++ session4/public/src/js/app.js
@@ -1,23 +1,35 @@
-
 var deferredPrompt;
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(function () {
-      console.log('Service worker registered!');
-    });
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(function () {
+    console.log("Service worker registered!");
+  });
 }
 
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
+window.addEventListener("beforeinstallprompt", function (event) {
+  console.log("beforeinstallprompt fired");
   event.preventDefault();
   deferredPrompt = event;
   return false;
 });
 
-setTimeout(function() {
-  console.log('This is executed once the timer is done!');
-}, 3000);
+var promise = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    // resolve("This is executed once the timer is done!");
+    reject({ code: 500, message: "An error occurred" });
+    // console.log("");
+  }, 3000);
+});
 
-console.log('This is executed right after setTimeout()');
+promise
+  .then(function (text) {
+    return text;
+  })
+  .then(function (newText) {
+    console.log(newText);
+  })
+  .catch(function (error) {
+    console.log(error.code);
+  });
+
+console.log("This is executed right after setTimeout()");
