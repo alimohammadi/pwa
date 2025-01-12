5847da60500e82895f6d286965b9b06362e8b53a
Ali Mohammadi <alimohammadi02321@gmail.com>
2025-01-12 16:04:50 +0330

    Feature/ add cache main pages for test cache

--- session5/public/sw.js
+++ session5/public/sw.js
@@ -1,14 +1,28 @@
+self.addEventListener("install", function (event) {
+  // console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open("static").then((cache) => {
+      console.log("[Service Worker] Precashing service worker");
 
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+      cache.add("/");
+      cache.add("/index.html");
+      cache.add("/src/js/app.js");
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener("activate", function (event) {
+  // console.log('[Service Worker] Activating Service Worker ....', event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ....', event);
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", function (event) {
+  // console.log('[Service Worker] Fetching something ....', event);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else return fetch(event.request);
+    })
+  );
 });
